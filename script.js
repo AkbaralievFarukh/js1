eeeedbddaa58a5a8ad2f7714b18010cae536c362
Farukh <akbfarukh@gmail.com>
2023-10-25 18:06:53 +0600

    midifed

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let a = 5
-let b = 8
-console.log(a + b)
+// выполнить (+, *, /, %)
+let a = 4
+let b = 55
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
+// вывести в консоль hello (ваше имя)
+a = 'Hello'
+b = 'Farukh'
+console.log(a + b);
+
+// вывести в консоль 10
+a = 5
+b = '5'
+console.log(a + +b)
+
+// вывести в консоль NaN
+let c = 2
+b = undefined
+console.log(c + b)
